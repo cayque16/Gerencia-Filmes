f4cb6e9c8bd629a15e545611c5bc39aeeeb76d0d
Cayque Santos <cayque_santos16@hotmail.com>
2020-06-18 16:14:13 -0300

    Adequando funcao que testa se ja existe um login salvo.

--- Filmes/app/src/main/java/br/com/flash/filmes/preferences/LoginPreferences.java
+++ Filmes/app/src/main/java/br/com/flash/filmes/preferences/LoginPreferences.java
@@ -33,7 +33,7 @@ public class LoginPreferences {
     }
 
     public boolean temLogin() {
-        return getLogin() != null;
+        return !(getLogin() == null);
     }
 
     private SharedPreferences getSharedPreferences() {
